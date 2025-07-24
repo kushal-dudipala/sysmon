b1c89c59672f8935f63893c83753839d11626e86
Kushal Dudipala <kushalreddyd@gmail.com>
2025-07-24 18:02:18 -0400

    wrap Cocoa FFI calls in safe Rust helpers

--- sysmon/src/cocoa_helpers.rs
+++ sysmon/src/cocoa_helpers.rs
@@ -2,40 +2,46 @@ use cocoa::appkit::NSMenuItem;
 use cocoa::base::{id, nil};
 use cocoa::foundation::{NSAutoreleasePool, NSString};
 use objc::{msg_send, sel, sel_impl};
+use crate::types::SendUiPtr;
 
-// These annotations confine the upstream objc macro’s noisy cfg to this file only.
-
-#[allow(unexpected_cfgs)]
-pub unsafe fn status_button(status_item: id) -> id {
-    msg_send![status_item, button]
+pub fn status_button(status_item: id) -> id {
+    unsafe { msg_send![status_item, button] }
 }
 
-#[allow(unexpected_cfgs)]
-pub unsafe fn set_button_title(button: id, s: &str) {
-    let title = NSString::alloc(nil).init_str(s);
-    let _: () = msg_send![button, setTitle: title];
+pub fn set_button_title(ptr: SendUiPtr, s: &str) {
+    unsafe {
+        let id = ptr.0.as_ptr() as cocoa::base::id;
+        let title = cocoa::foundation::NSString::alloc(nil).init_str(s);
+        let _: () = objc::msg_send![id, setTitle: title];
+    }
 }
 
-#[allow(unexpected_cfgs)]
-pub unsafe fn new_menu_item_with_title(s: &str) -> id {
-    let _pool = NSAutoreleasePool::new(nil); // defensively pool ObjC allocs here too
-    let item = NSMenuItem::new(nil).autorelease();
-    let _: () = msg_send![item, setTitle: NSString::alloc(nil).init_str(s)];
-    item
+pub fn set_menu_item_title(ptr: SendUiPtr, s: &str) {
+    unsafe {
+        let id = ptr.0.as_ptr() as cocoa::base::id;
+        let ns = cocoa::foundation::NSString::alloc(nil).init_str(s);
+        let _: () = objc::msg_send![id, setTitle: ns];
+    }
 }
 
-#[allow(unexpected_cfgs)]
-pub unsafe fn menu_add_item(menu: id, item: id) {
-    let _: () = msg_send![menu, addItem: item];
+pub fn new_menu_item_with_title(s: &str) -> id {
+    unsafe {
+        let _pool = NSAutoreleasePool::new(nil);
+        let item = NSMenuItem::new(nil).autorelease();
+        let title = NSString::alloc(nil).init_str(s);
+        let _: () = msg_send![item, setTitle: title];
+        item
+    }
 }
 
-#[allow(unexpected_cfgs)]
-pub unsafe fn status_item_set_menu(status_item: id, menu: id) {
-    let _: () = msg_send![status_item, setMenu: menu];
+pub fn menu_add_item(menu: id, item: id) {
+    unsafe {
+        let _: () = msg_send![menu, addItem: item];
+    }
 }
 
-#[allow(unexpected_cfgs)]
-pub unsafe fn set_menu_item_title(item: id, s: &str) {
-    let ns = NSString::alloc(nil).init_str(s);
-    let _: () = msg_send![item, setTitle: ns];
+pub fn status_item_set_menu(status_item: id, menu: id) {
+    unsafe {
+        let _: () = msg_send![status_item, setMenu: menu];
+    }
 }
