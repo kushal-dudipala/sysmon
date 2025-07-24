9b0607a093d9038b04dfbdb7a1da34020dac8761
Kushal Dudipala <kushalreddyd@gmail.com>
2025-07-24 18:02:55 -0400

    add SendUiPtr wrapper for pointer usage

---
+++ sysmon/src/types.rs
@@ -0,0 +1,19 @@
+use objc::runtime::Object;
+use std::ptr::NonNull;
+
+#[derive(Copy, Clone)]
+pub struct SendUiPtr(pub NonNull<Object>);
+
+impl SendUiPtr {
+    pub fn new(ptr: *mut Object) -> Option<Self> {
+        NonNull::new(ptr).map(Self)
+    }
+
+    pub fn as_ptr(self) -> *mut Object {
+        self.0.as_ptr()
+    }
+}
+
+
+// SAFETY: Pointer assumed valid and pinned to main thread.
+unsafe impl Send for SendUiPtr {}
